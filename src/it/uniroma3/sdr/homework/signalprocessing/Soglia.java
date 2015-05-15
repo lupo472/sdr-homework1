d659fd44ec0d12eaa153f96057de9560758ce649
Luca Antilici <luc.antilici@stud.uniroma3.it>
2015-05-15 13:26:24 +0200

    Modifica del calcolo di z nel ciclo for

--- src/it/uniroma3/sdr/homework/signalprocessing/Soglia.java
+++ src/it/uniroma3/sdr/homework/signalprocessing/Soglia.java
@@ -11,14 +11,12 @@ public class Soglia {
 	public static double soglia(Signal signal, double snr,int num_prove, double Pfa){
 		double[] z = new double[signal.getLength()]; //array delle energie del segnale in ogni prova
 		double soglia;
-		double sum = 0;
+	
 		for(int i = 0;i<signal.getLength();i++){
 			Noise noise = new Noise(snr,signal.getLength());
-			double pri_noise = noise.getParteReale()[i] + noise.getParteImmaginaria()[i];
-			z[i] = sum + Math.pow(Math.abs(pri_noise),2);
+			double pri_noise = noise.getParteReale()[i] + noise.getParteImmaginaria()[i];//somma parte immaginaria e reale del rumore
+			z[i] = Math.pow(Math.abs(pri_noise),2);//mi salvo in z ogni elemento della sommatoria per l'energia del segnale
 		}
-		
-		
 		soglia = MeanVar.mean(z)+(Math.sqrt(2*MeanVar.var(z))*Erf.erfInv(1-2*Pfa));
 		return soglia;
 	}
